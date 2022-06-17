0d3e2f252e513ffdd7a15c264864098b00fda57c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-06-17 14:58:01 -0400

    fixed sntax errors in Executable Time

--- Execution_Time.rb
+++ Execution_Time.rb
@@ -1,4 +1,4 @@
-#Phase I-O(n^2) quadratic time
+#Phase I-O(n^2) 
 def my_min_1a(arr)
 
   arr.each do |ele1|
@@ -21,7 +21,7 @@ end
 
 
 
-
+#PHASE I: O(n**2)
 def largest_contiguous_subsum1(arr)
   subarrays = []
 
@@ -31,17 +31,16 @@ def largest_contiguous_subsum1(arr)
     end
   end
 
-  subarrays.inject {|acc, el| acc if acc.sum < el.sum}.sum
+  subarrays.inject{|acc, el| acc if acc.sum < el.sum}.sum
 end
 
-#O(n) linear time
-#O(1) constant space
+#O(n) 
 def largest_contiguous_subsum2(arr)
   largest_sum = arr.first
   current_sum= arr.first
 
   arr.drop(1).each do |ele|
-    current = 0 if current_sum < 0
+    current_sum = 0 if current_sum < 0
     current_sum += ele
     largest_sum = current_sum if current_sum > largest_sum
   end
